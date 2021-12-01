f68ca47ef15e6234dc65eb5270d2da337b3b946e
Mark Holmes <github@markholmes.org>
2021-11-30 23:08:14 -0800

    2021 bb lets goooo

---
+++ 2021/mix.exs
@@ -0,0 +1,22 @@
+defmodule Aoc.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :aoc,
+      version: "0.1.0",
+      elixir: "~> 1.12.3",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps, do: []
+end
